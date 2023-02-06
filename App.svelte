8ca21829611d0387bb187a6d066945e1c67ef0de
Simon Porritt <simon@jsplumbtoolkit.com>
2023-02-06 22:16:46 +1100

    initial import

---
+++ App.svelte
@@ -0,0 +1,25 @@
+
+<script>
+    import {onMount} from "svelte"
+    import {FlowchartComponent, PaletteComponent, InspectorComponent} from "@jsplumb-components/flowchart-svelte"
+
+    let flowchartBuilder
+
+    onMount(async () => {
+        flowchartBuilder.loadUrl("./copyright.json", () => {
+            flowchartBuilder.zoomToFit()
+        })
+    })
+
+</script>
+
+
+<div id="canvas">
+    <FlowchartComponent id="myFlowchart" bind:this={flowchartBuilder}/>
+</div>
+
+<div id="rhs">
+    <PaletteComponent flowchartId="myFlowchart"></PaletteComponent>
+    <InspectorComponent flowchartId="myFlowchart"></InspectorComponent>
+</div>
+
